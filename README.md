6fd2bd2b3f2ee03ea1e4409597167fb0ca03df3e
InYourWalls <notinyourwalls@gmail.com>
2022-11-28 18:44:34 -0500

    finish functionality, add readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# iptool_v2
+The new version of [ip.inyourwalls.net](https://ip.inyourwalls.net/), now with more Rust.
+
+## Features
+* Templating with [Tera](https://github.com/Keats/tera).
+* That's it.
+
+This program is written primarily for my own purposes, but you're welcome to compile and use it if you wish.
+It's built with [Rocket](https://github.com/SergioBenitez/Rocket), and uses its template library. Templates
+are loaded from `/templates` in your server folder.
--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,30 @@
  * A simple website that shows the user their IP. *
  * ---------------------------------------------- */
 
-use std::net::{IpAddr, Ipv4Addr};
 // Imports:
 use rocket::{get, Request, routes};
 use rocket_dyn_templates::{context, Template};
+use std::net::{IpAddr, Ipv4Addr};
 
 #[get("/")]
 fn ip(req: Request) -> Template {
-    // Get the client IP from the X-Real-IP header, or 0.0.0.0 if unknown.
+    // Get the client IP from the X-Real-IP header.
     let addr = req.real_ip();
     if let Some(address) = addr {
+        // Return the IP address.
         Template::render("index", context! { client_address: address.to_string() })
     } else {
-        Template::render("index", context! { client_address: "Error" })
+        // We don't know what it is.
+        Template::render("index", context! { client_address: String::from("Unknown") })
     }
 }
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
-    println!("Starting server...");
-
     // Launch Rocket.
     let _ = rocket::build()
         .mount("/", routes![ip])
+        .attach(Template::fairing())
         .launch()
         .await?;
 
